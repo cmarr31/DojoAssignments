8a03b6d562bf18117380c8e27fc997035ba73d77
Mauricio Ruanova-Hurtado <mruanova@yahoo.com>
2017-05-07 10:53:53 -0500

    update coding dojo assignments

---
+++ React/script.jsx
@@ -0,0 +1,7 @@
+/*
+babel.min.js:24 You are using the in-browser Babel transformer. 
+Be sure to precompile your scripts for production - https://babeljs.io/docs/setup/
+u @ babel.min.js:24
+E @ babel.min.js:1
+Inline Babel script:52 (5) [2, 4, 6, 8, 10]
+*/
