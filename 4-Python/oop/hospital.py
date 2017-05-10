12b420a0778f1e89345aad08cbc0fe029ddc2adb
Mauricio Ruanova-Hurtado <mruanova@yahoo.com>
2017-05-10 16:58:02 -0500

    update coding dojo assignments

--- 4-Python/oop/hospital.py
+++ 4-Python/oop/hospital.py
@@ -12,46 +12,56 @@ Hospital
     Change bed number for that patient back to none.
 '''
 class Patient(object):
-    def __init__(self, id_number, name, allergies, bed_number = 0):
+    def __init__(self, id_number, name, allergies, bed_number = -1):
         self.id = id_number
         self.name = name
         self.allergies = allergies
         self.bed_number = bed_number
+    def set_bed_number(self, bed_number):
+        self.bed_number = bed_number
+        return self
     def display_all(self):
         print "ID:", self.id
         print "Name:", self.name
         print "Allergies:", self.allergies
         print "Bed Number:", self.bed_number
         print " "
+        return self
 class Hospital(object):
     def __init__(self, patients, name, capacity = 3):
         self.patients = patients
         self.name = name
-        self.capacity = capacity
         self.occupancy = len(self.patients)
-        self.beds = [5]
+        self.beds = []
+        for i in range(capacity):
+            self.beds.append(-1)
     def admit(self, patient):
-        if self.occupancy < self.capacity:
-            self.patients.append(patient) # Add a patient to the list of patients. 
-            #self.queue_size = len(self.calls)
-            print "admited"
-            self.occupancy += 1
-            # Assign the patient a bed number. 
-            # If the length of the list is >= the capacity do not admit the patient.
-            # Return a message either confirming that admission is complete or saying the hospital is full.
+        if self.occupancy < len(self.beds):
+            for i in range(len(self.beds)):
+                if self.beds[i] == -1: # available
+                    print "admited", i # Return a message either confirming that admission is complete or saying the hospital is full.
+                    # patient.set_bed_number(bed_number)  # Assign the patient a bed number.
+                    self.patients.append(patient) # Add a patient to the list of patients. 
+                    self.beds[i] = i
+                    self.occupancy += 1     
+                    return i
         else:
-            print "rejected"
-        return self
+            print "rejected" # If the length of the list is >= the capacity do not admit the patient.
+        return -1
     def discharge(self, patient):
-        print "discharged"
-        self.occupancy -= 1
-        self.patients.pop(0) # Discharge: look up and remove a patient from the list of patients.
-        # Change bed number for that patient back to none.
+        for i in range(len(self.beds)):
+            if self.beds[i] == patient.bed_number:
+                print "discharged", i
+                patient.set_bed_number(0) # Change bed number for that patient back to none.
+                self.patients.pop(0) # Discharge: look up and remove a patient from the list of patients.
+                self.beds[i] = -1 # available
+                self.occupancy -= 1   
+                return i
         return self
     def display_all(self):
         print "Patients:", len(self.patients)
         print "Name:", self.name
-        print "Capacity:", self.capacity
+        print "Capacity:", len(self.beds)
         print "Occupancy:", self.occupancy
         print " "
         return self
@@ -60,4 +70,34 @@ p1 = Patient(1, "Mau", "most drugs").display_all()
 p2 = Patient(2, "Alex", "apples").display_all()
 p3 = Patient(3, "Santi", "sugar").display_all()
 p4 = Patient(4, "Gerardo", "none").display_all()
-h = Hospital([p1, p2, p3],"General Hospital").display_all().admit(p4).display_all().discharge(p1).display_all().admit(p4).display_all()
+h = Hospital([],"General Hospital")
+
+bed_number = h.admit(p1)
+if bed_number >= 0:
+    p1.set_bed_number(bed_number)
+
+bed_number = h.admit(p2)
+if bed_number >= 0:
+    p2.set_bed_number(bed_number) 
+
+bed_number = h.admit(p3)
+if bed_number >= 0:
+    p3.set_bed_number(bed_number) 
+
+h.display_all()
+
+bed_number = h.admit(p4)
+if bed_number >= 0:
+    p4.set_bed_number(bed_number) 
+
+h.display_all()
+
+h.discharge(p1)
+
+h.display_all()
+
+bed_number = h.admit(p4)
+if bed_number >= 0:
+    p4.set_bed_number(bed_number) 
+
+h.display_all()
