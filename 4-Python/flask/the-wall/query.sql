f6174a640a938c46eeffe9a3fc6fe1a1e678eba2
Mauricio Ruanova-Hurtado <mruanova@yahoo.com>
2017-05-18 14:07:11 -0500

    update coding dojo assignments

--- 4-Python/flask/the-wall/query.sql
+++ 4-Python/flask/the-wall/query.sql
@@ -1,4 +1,5 @@
 use the_wall_db;
 select * from users;
-select * from messages;
+select * from messages where created_at between date_add(NOW(),INTERVAL -30 MINUTE) and NOW();
 select * from comments;
+DELETE FROM comments WHERE id =1;
