d20945ddfff99076e3a4504ff3b15c0db63cb8d7
Mauricio Ruanova-Hurtado <mruanova@yahoo.com>
2017-05-19 15:47:53 -0500

    update coding dojo assignments

---
+++ 4-Python/django/random_number_generator/apps/random_number_generator_app/apps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class RandomNumberGeneratorAppConfig(AppConfig):
+    name = 'random_number_generator_app'
