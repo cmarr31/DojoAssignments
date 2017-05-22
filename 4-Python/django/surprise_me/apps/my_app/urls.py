26f3a5db66378081f3c3b0c46380579d46549e0c
Mauricio Ruanova-Hurtado <mruanova@yahoo.com>
2017-05-22 14:21:05 -0500

    update coding dojo assignments

--- 4-Python/django/surprise_me/apps/my_app/urls.py
+++ 4-Python/django/surprise_me/apps/my_app/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index)
+    url(r'^$', views.index),
+    url(r'^submit$', views.submit),
+    url(r'^results$', views.results)
 ]
--- 4-Python/django/surprise_me/apps/my_app/views.py
+++ 4-Python/django/surprise_me/apps/my_app/views.py
@@ -1,16 +1,28 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render, redirect, HttpResponse
-
-# Create your views here.
-
+VALUES = ['strange', 'weird', 'unusual', 'rare', 'different', 'wicked', 'odd', 'crazy', 'eccentric', 'queer', 'singular', 'funky', 'freaky', 'curious', 'uncanny']
+'''
+Surprise Me! 
+Make an app that serves up whichever number of random items the user requests.
+If the user enters '3' display 3 items on the next page.
+At the top of your views.py, under your module imports add: VALUES = ['populate this list with various strings']
+When a user enters a number and submits the form, shuffle the list of strings. 
+You can access the above list from any view function. 
+Return to the template n number of elements from the front of the list, where n is the value the user entered.
+Display the strings in the template.
+'''
 def index(request):
-    # return HttpResponse("response")
     return render(request, 'index.html')
 
-def results(request, results):
-    # VALUES = ['populate this list with various strings']
-    print results;
-    context = {'results':results}
+def results(request):
+    # shuffle array 
+    # for loop
+    # HOW TO ADD? context.add(key,value) context = {'results':request.session['number']} 
+    context = {'values':VALUES}
     return render(request, 'results.html', context)
+    
+def submit(request):
+    if request.method == "POST":
+        request.session['number'] = request.POST['number']
+    return redirect('/results')
