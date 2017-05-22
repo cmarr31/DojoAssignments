2c3b252a81b027f6c763eedb2ebe8f46d3a995df
Mauricio Ruanova-Hurtado <mruanova@yahoo.com>
2017-05-22 15:58:27 -0500

    update coding dojo assignments

--- 4-Python/django/landscapes/apps/my_app/views.py
+++ 4-Python/django/landscapes/apps/my_app/views.py
@@ -1,6 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect, HttpResponse
+import random
+'''
+Landscapes:
+Create an app that displays a different image to the user depending upon the value the user enters as a url parameter
+Display a different landscape photo depending on the value the user passed in as a route parameter.
+Have two templates
+Display a different landscape photo for each range of values:
+• 1-10: A landscape with snow
+• 11-20: A landscape in the desert
+• 21-30: A landscape in a forest
+• 31-40: A landscape in a vineyard
+• 41-50: A tropical landscape
+'''
+def index(request):
+    return render(request, 'index.html')
+def results(request, param):
+    context = {'param': param}
+    return render(request, 'results.html', context)
